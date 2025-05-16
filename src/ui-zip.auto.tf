679acb8af8aca58a6a2e4bfb0bb5d54e5050482c
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-16 06:06:35 +0000

    Update Jenkinsfile (floward-exercise)

---
+++ src/ui-zip.auto.tf
@@ -0,0 +1,7 @@
+# Module: floward-exercise
+locals {
+  floward-exercise_commitHash = "765db033f0d31c73840182cb05c35d792d865c5b"
+  floward-exercise_commitDate = "2025-05-16T11:35:56+05:30"
+  floward-exercise_bucketName = "tf-test-1"
+  floward-exercise_objectName = "HCLCODE_Test/floward-exercise/v20250516-060623.zip"
+}
